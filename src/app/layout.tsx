09385b8b25b35ed03ddbfa92aaaaee632ceba437
devmusiczz <devr69080@gmail.com>
2025-06-09 15:00:10 +0530

    first commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,45 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "HotelHub",
+  description: "Hotel Booking Platform",
+  icons: { icon : '/logo.svg' },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+
+    <html lang="en" suppressHydrationWarning >
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+
+        <main className="flex flex-col min-h-screen bg-secondary">
+
+          <section className="flex-grow">
+            {children}
+          </section>
+        </main>
+
+      </body>
+    </html>
+
+  );
+}
